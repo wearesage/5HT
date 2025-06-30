441ea9e1d745d0acefd7806ec37269fc5845ceda
Zach Winter <contact@zachwinter.com>
2025-06-29 21:27:41 -0400

    Initial commit.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,4 @@ import { createApp } from "@wearesage/vue";
 import * as generatedRoutes from "./router/generated-routes";
 import App from "./App.vue";
 
-// 🔥 SAGE ROUTER - NO MORE VUE ROUTER BULLSHIT!
-// Routes are auto-generated from src/pages/**/*.vue
-// Auth checking is pure reactive in Root.vue
-
-// Pass the generated routes to createApp
 await createApp(App, { routes: generatedRoutes });
